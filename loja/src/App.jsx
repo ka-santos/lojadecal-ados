039ef25ab3a4b09d7e435150c7627a65175daf55
ka-santos <k.carvalho@aluno.ifsp.edu.br>
2025-09-01 17:51:22 -0300

    site inicializado

---
+++ loja/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Topo from './components/topo'
+
+function App() {
+  
+  return (
+    <>
+      <header>
+        <Topo/>
+      </header>
+    </>
+  )
+}
+
+export default App
---
+++ loja/src/components/Banner/index.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+import banner from '../../assets/banner.jpg'
+import bannerMobile from '../../assets/banner_mobile.jpg'
